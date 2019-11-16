c6a5f287ee039425c73bfe25c25ab2d4e89fc465
lpaqkosw <jaehoonlee1994@gmail.com>
2019-11-16 17:37:33 +0900

    nameToCode function removed/ stock_data to english

--- background.js
+++ background.js
@@ -2,7 +2,7 @@ chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
         var xhr = new XMLHttpRequest();
 
-        xhr.open("GET", 'http://13.125.105.168/' + request.code, false);
+        xhr.open("GET", 'http://13.125.105.168/' + request.name, false);
         xhr.send();
 
         var result = xhr.responseText;
